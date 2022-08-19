800aa187f280d8b80ae3095d79b9437045839e3c
Luis071727 <lubenitezmu@gmail.com>
2022-08-19 18:11:14 -0500

    15

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -83,10 +83,8 @@ function estaEnRango(numero) {
   // Devuelve "true" si "numero" es menor que 50 y mayor que 20
   // De lo contrario, devuelve "false"
   // Tu código:
-  if (numero < 50) { 
-    if (numero >= 20)
-      return true;
-        
+  if (numero < 50 && numero > 20) { 
+    return true  
   } else {
     return false;
   }
@@ -137,7 +135,7 @@ function operadoresLogicos(num1, num2, num3) {
   } else if (num1 < 0 || num2 < 0 || num3 < 0) {
     return "Hay negativos"
   } else if (num3 > num1 && num3 > num2) {
-    num3++
+    num3 =+ 1;
     return num3
   } else {
     return false
